ab7d699d56571dd4dbd1e71ba1e1bc5e5b67d29d
jayant-TA <105401876+jayant-TA@users.noreply.github.com>
2022-05-25 19:09:39 +0530

    Update ApiDispensaryUpdateController.php

--- app/Http/Controllers/ApiDispensaryUpdateController.php
+++ app/Http/Controllers/ApiDispensaryUpdateController.php
@@ -99,9 +99,13 @@
 
 				if(!empty($postdata['store_meta'])){
 					$postdata['store_meta'] = serialize($postdata['store_meta']);
+				}else{
+					$postdata['store_meta'] = '';
 				}
 				if(!empty($postdata['assign_user'])){
 					$postdata['assign_user'] = serialize($postdata['assign_user']);
+				}else{
+					$postdata['assign_user'] = '';
 				}
 		        // unset($postdata['file']);
 		    }
@@ -136,18 +140,22 @@
 									  ->first();
 					$result['data'] = $update_disp;
 				}
+				
+				if(!empty($result['data']->id )){
+					DB::table('dispensaries_users')->where('dispansary_id',$result['data']->id )->delete();
+					if(!empty($result['data']->assign_user)){
 
-				if(!empty($result['data']->id && !empty($result['data']->assign_user))){
-					$assignUserData = unserialize($result['data']->assign_user);
-					$updateUserData = [];
-					if(!empty($assignUserData)){
-						foreach($assignUserData as $assignUserKey => $assignUserVal){
-							$updateUserData[$assignUserKey]['user_id'] = $assignUserVal;
-							$updateUserData[$assignUserKey]['dispansary_id'] = $result['data']->id;
-						}
-						if(!empty($updateUserData)){
-
-							DB::table('dispensaries_users')->insert($updateUserData);
+						$assignUserData = unserialize($result['data']->assign_user);
+						$updateUserData = [];
+						if(!empty($assignUserData)){
+							foreach($assignUserData as $assignUserKey => $assignUserVal){
+								$updateUserData[$assignUserKey]['user_id'] = $assignUserVal;
+								$updateUserData[$assignUserKey]['dispansary_id'] = $result['data']->id;
+							}
+							
+							if(!empty($updateUserData)){
+								DB::table('dispensaries_users')->insert($updateUserData);
+							}
 						}
 					}
 
